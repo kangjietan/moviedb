7640b03f324518ee382c0258fd6b2e77af734e79
kangjietan <kangjietan@gmail.com>
2020-06-11 00:38:52 -0700

    Retrieve search results from store

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Results() {
+import { connect, useSelector } from 'react-redux';
+
+
+function Results(props) {
+  const results = props.searchResults.results || [];
   return (
     <div>
       <h1>Results Page</h1>
+      <div>
+        {results.map((movie) => <div key={movie.id}>{movie.title}</div>)}
+      </div>
     </div>
   );
 }
 
-export default Results;
+const mapStateToProps = state => ({
+  searchResults: state.search.searchResults
+});
+
+export default connect(mapStateToProps)(Results);
