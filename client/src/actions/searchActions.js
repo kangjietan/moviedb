313d2e6cba163eb7d39419ffa3c83ea605524547
kangjietan <kangjietan@gmail.com>
2020-06-11 00:02:03 -0700

    Add search API action

--- client/src/actions/searchActions.js
+++ client/src/actions/searchActions.js
@@ -1,9 +1,29 @@
 import * as actions from "../actions/types.js";
+import axios from "axios";
+
+const serverUrl = "http://localhost:3000";
 
 export const updateSearch = (searchData) => (dispatch) => {
-  console.log(searchData);
   dispatch({
     type: actions.UPDATE_SEARCH,
     payload: searchData,
   });
+};
+
+export const searchAPI = (query) => (dispatch) => {
+  console.log("Fetching form API");
+  axios
+    .get(`${serverUrl}/tmdb/search`, {
+      params: { query },
+    })
+    .then((response) => {
+      console.log("REDUX ACTION", response);
+      dispatch({
+        type: actions.SEARCH_API,
+        payload: response.data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
