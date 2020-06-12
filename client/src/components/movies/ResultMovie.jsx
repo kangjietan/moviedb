9a6296433b8480a2f84074f18f3d0461eba937ad
kangjietan <kangjietan@gmail.com>
2020-06-11 19:07:55 -0700

    Render modal on card click

--- client/src/components/movies/ResultMovie.jsx
+++ client/src/components/movies/ResultMovie.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import PropTypes from 'prop-types';
+
+Modal.setAppElement('#app');
 
 function ResultMovie({ movie }) {
   const imageUrl = movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : "./no-image.jpg";
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
-    <div className="card m-3" style={{ width: '18rem' }}>
+    <div className="card m-3" style={{ width: '18rem' }} onClick={() => setModalIsOpen(true)}>
       <img src={imageUrl} className="card-img-top" />
       <div className="card-body">
         <h4 className="card-title">{movie.title}</h4>
       </div>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} shouldCloseOnOverlayClick={true}>
+
+      </Modal>
     </div>
   );
 }
