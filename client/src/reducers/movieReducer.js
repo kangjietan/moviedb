d469ec94f2949bfb5c1afa5c84a7530f6f98dae6
kangjietan <kangjietan@gmail.com>
2020-06-22 23:30:29 -0700

    Don't add movie if it already exists in list

--- client/src/reducers/movieReducer.js
+++ client/src/reducers/movieReducer.js
@@ -8,12 +8,32 @@ let initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case actions.ADD_WATCHED_MOVIE:
+      let watchedMovie = action.payload;
+
+      for (let i = 0; i < state.watchedList.length; i++) {
+        if (state.watchedList[i].id === watchedMovie.id) {
+          return {
+            ...state,
+          };
+        }
+      }
+
       return {
         ...state,
         watchedList: [...state.watchedList, action.payload],
       };
 
     case actions.ADD_TO_WATCH_MOVIE:
+      let toWatchMovie = action.payload;
+
+      for (let i = 0; i < state.toWatchList.length; i++) {
+        if (state.toWatchList[i].id === toWatchMovie.id) {
+          return {
+            ...state,
+          };
+        }
+      }
+
       return {
         ...state,
         toWatchList: [...state.toWatchList, action.payload],
