35ab25c7faf25f100caa5d4f2a2cbc15a70647a5
kangjietan <kangjietan@gmail.com>
2020-06-23 23:26:42 -0700

    Handle page refreshes on react router paths

--- server/server.js
+++ server/server.js
@@ -22,4 +22,8 @@ app.get("/tmdb/search", (req, res) => {
     });
 });
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 app.listen(PORT, () => console.log("Listening on port: " + PORT));
