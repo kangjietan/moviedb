4fedc72fb2e8aae109effc74772f963130f799a2
kangjietan <kangjietan@gmail.com>
2020-06-26 23:32:56 -0700

    Render watched movie component for each listing.
    When user clicks on the watched button.

--- client/src/components/WatchedList.jsx
+++ client/src/components/WatchedList.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function WatchedList() {
+import WatchedMovie from './movies/WatchedMovie.jsx';
+
+import { connect } from 'react-redux';
+
+function WatchedList({ watchedList }) {
+  let movies = Object.keys(watchedList);
   return (
-    <div>
-      <h1>WatchedList Page</h1>
+    <div className="container">
+      {movies.map((id) => <WatchedMovie key={id} movie={watchedList[id]} />)}
     </div>
   );
 }
 
-export default WatchedList;
+const mapStateToProps = state => ({
+  watchedList: state.movie.watchedList
+});
+
+export default connect(mapStateToProps, null)(WatchedList);
