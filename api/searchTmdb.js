b2331a4b7e4548866b62acd86de685b26d7f35cc
kangjietan <kangjietan@gmail.com>
2020-06-28 18:48:26 -0700

    Add get genres to API helper.

--- api/searchTmdb.js
+++ api/searchTmdb.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const { TMDB_API_KEY } = process.env;
 
-const searchTmdb = (query) => {
+const searchMovies = (query) => {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}/&query=${query}`;
 
   const options = {
@@ -27,4 +27,30 @@ const searchTmdb = (query) => {
   });
 };
 
-module.exports = searchTmdb;
+const getGenres = () => {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${TMDB_API_KEY}&language=en-US`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: TMDB_API_KEY,
+      "X-Requested-With": "XMLHttpRequest",
+    },
+    url,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(options)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = {
+  searchMovies,
+  getGenres,
+};
