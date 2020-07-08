f764d2a6022af8c13c093fa168e7590993880c38
kangjietan <kangjietan@gmail.com>
2020-07-08 00:50:00 -0700

    Add modal to watched movie component

--- client/src/components/movies/WatchedMovie.jsx
+++ client/src/components/movies/WatchedMovie.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-import { connect } from 'react-redux';
+import WatchedListModal from './modal/WatchedListModal.jsx';
 
 const MovieContainer = styled.div`
   display: flex;
@@ -19,48 +19,64 @@ const MovieImgContainer = styled.div`
   width: 9%;
   margin: auto;
   margin-right: 20px;
+  cursor: pointer;
 `;
 
 const MovieGenreContainer = styled.div`
   width: 25%;
   text-align: center;
+  margin: auto;
 `;
 
 const MovieTitleContainer = styled.div`
   width: 25%;
   text-align: center;
+  margin: auto;
 `;
 
 const MovieOverviewContainer = styled.div`
   width: 40%;
   text-align: center;
+  margin: auto;
 `;
 
 function WatchedMovie({ movie, genres }) {
   const imageUrl = movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : "./no-image.jpg";
 
+  const [contentModalIsOpen, setContentModalIsOpen] = useState(false);
+
   let movieGenres = movie.genre_ids.map((id) => `${genres[id]}`);
 
   return (
-    <MovieContainer>
-      <MovieImgContainer>
-        <img src={imageUrl} className="img-fluid" />
-      </MovieImgContainer>
-      <MovieTitleContainer>
-        {movie.title}
-      </MovieTitleContainer>
-      <MovieOverviewContainer>
-        {movie.overview}
-      </MovieOverviewContainer>
-      <MovieGenreContainer>
-        {movieGenres.map((genre) => <div>{genre}</div>)}
-      </MovieGenreContainer>
-    </MovieContainer>
+    <div>
+      <MovieContainer>
+        <MovieImgContainer onClick={() => { setContentModalIsOpen(true) }}>
+          <img src={imageUrl} className="img-fluid" />
+        </MovieImgContainer>
+        <MovieTitleContainer>
+          {movie.title}
+        </MovieTitleContainer>
+        <MovieOverviewContainer>
+          {movie.overview}
+        </MovieOverviewContainer>
+        <MovieGenreContainer>
+          {movieGenres.map((genre, idx) => <div key={idx}>{genre}</div>)}
+        </MovieGenreContainer>
+      </MovieContainer>
+      <WatchedListModal
+        imageUrl={imageUrl}
+        movieGenres={movieGenres}
+        movie={movie}
+        contentModalIsOpen={contentModalIsOpen}
+        setContentModalIsOpen={setContentModalIsOpen}
+      />
+    </div>
   );
 }
 
-const mapStateToProps = state => ({
-  genres: state.search.genres,
-});
+WatchedMovie.propTypes = {
+  movie: PropTypes.object.isRequired,
+  genres: PropTypes.object.isRequired,
+}
 
-export default connect(mapStateToProps)(WatchedMovie);
+export default WatchedMovie;
