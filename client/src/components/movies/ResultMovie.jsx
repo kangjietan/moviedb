e80013f67bc113fd3b149bccf11d7a72c3653b8a
kangjietan <kangjietan@gmail.com>
2020-07-08 00:07:40 -0700

    Bring in refactored trailer modal component

--- client/src/components/movies/ResultMovie.jsx
+++ client/src/components/movies/ResultMovie.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import TrailerModal from './modal/TrailerModal.jsx';
+
 import Modal from 'react-modal';
 import axios from 'axios';
 
@@ -123,31 +125,6 @@ class ResultMovie extends Component {
       }
     };
 
-    const trailerModalStyle = {
-      overlay: {
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        right: '0',
-        bottom: '0',
-        backgroundColor: 'rgba(255, 255, 255, 0.75)',
-      },
-      content: {
-        position: 'absolute',
-        top: '5vh',
-        left: '10vh',
-        right: '10vh',
-        bottom: '5vh',
-        border: '1px solid #ccc',
-        background: '#fff',
-        overflow: 'auto',
-        WebkitOverflowScrolling: 'touch',
-        borderRadius: '4px',
-        outline: 'none',
-        padding: '20px',
-      }
-    };
-
     return (
       <div>
         <div className="card m-3" style={{ width: '18rem', cursor: 'pointer' }} onClick={() => this.setModalIsOpen(true, 'content')}>
@@ -193,21 +170,11 @@ class ResultMovie extends Component {
             </div>
           </div>
         </Modal>
-        <Modal isOpen={trailerModalIsOpen} onRequestClose={() => this.setModalIsOpen(false, 'trailer')} style={trailerModalStyle}>
-          <button
-            className="btn btn-link"
-            style={{ position: 'absolute', top: '0px', right: '0px', padding: '0px' }}
-            onClick={() => this.setModalIsOpen(false, 'trailer')}>
-            X
-          </button>
-          <iframe
-            style={{ height: '85vh', width: '100%' }}
-            allowFullScreen="allowFullScreen"
-            allow="autoplay"
-            title="trailer"
-            src={`https://www.youtube.com/embed/${movieTrailerUrl}?autoplay=1`}
-          />
-        </Modal>
+        <TrailerModal
+          movieTrailerUrl={movieTrailerUrl}
+          trailerModalIsOpen={trailerModalIsOpen}
+          setTrailerModalIsOpen={() => { this.setModalIsOpen(false, 'trailer') }}
+        />
       </div>
     );
   }
