94df53ea5827f0efc7037b773d8ae0a2f36b1c9c
kangjietan <kangjietan@gmail.com>
2020-07-11 00:18:43 -0700

    Render trending movie list in component

--- client/src/components/homepage/TrendingMovies.jsx
+++ client/src/components/homepage/TrendingMovies.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TrendingMovies() {
+import HomePageMovie from './HomePageMovie';
+
+function TrendingMovies({ dayTrendingMoviesList }) {
+  let list = dayTrendingMoviesList ? dayTrendingMoviesList : [];
+
   return (
     <div>
       <h2>Trending</h2>
+      <div style={{ height: "30vh" }}>
+        <HomePageMovie list={list} />
+      </div>
     </div>
   );
 }
