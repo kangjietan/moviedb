af5f706d228abe46fd0c52e62450fb0a2fb16127
kangjietan <kangjietan@gmail.com>
2020-07-10 18:40:29 -0700

    Create trending movies component

---
+++ client/src/components/homepage/TrendingMovies.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TrendingMovies() {
+  return (
+    <div>
+      <h2>Trending</h2>
+    </div>
+  );
+}
+
+TrendingMovies.propTypes = {
+
+};
+
+export default TrendingMovies;
