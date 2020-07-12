fe6ca89bbbc17f27db7de10235f038378b3f3237
kangjietan <kangjietan@gmail.com>
2020-07-11 17:27:07 -0700

    Pass down genres to result movie component

--- client/src/components/homepage/PopularMovies.jsx
+++ client/src/components/homepage/PopularMovies.jsx
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 
 import HomePageMovie from './HomePageMovie';
 
-function PopularMovies({ popularMovieList }) {
-  console.log(popularMovieList);
+function PopularMovies({ popularMovieList, genres }) {
   let list = popularMovieList ? popularMovieList : [];
   return (
     <div>
       <h2>What's Popular</h2>
-      <div style={{ height: "30vh" }}>
-        <HomePageMovie list={list} />
+      <div style={{ height: "35rem" }}>
+        <HomePageMovie list={list} genres={genres} />
       </div>
     </div>
   );
--- client/src/components/homepage/TrendingMovies.jsx
+++ client/src/components/homepage/TrendingMovies.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 import HomePageMovie from './HomePageMovie';
 
-function TrendingMovies({ dayTrendingMoviesList }) {
+function TrendingMovies({ dayTrendingMoviesList, genres }) {
   let list = dayTrendingMoviesList ? dayTrendingMoviesList : [];
 
   return (
     <div>
       <h2>Trending</h2>
       <div style={{ height: "30vh" }}>
-        <HomePageMovie list={list} />
+        <HomePageMovie list={list} genres={genres} />
       </div>
     </div>
   );
