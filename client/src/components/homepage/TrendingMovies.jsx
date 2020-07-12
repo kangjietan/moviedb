d1733bbb6307790dd0103ba0e3ee4ec9ed56766a
kangjietan <kangjietan@gmail.com>
2020-07-11 18:26:11 -0700

    Render list depending on which button is clicked

--- client/src/components/homepage/TrendingMovies.jsx
+++ client/src/components/homepage/TrendingMovies.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import HomePageMovie from './HomePageMovie';
 
-function TrendingMovies({ dayTrendingMoviesList, genres }) {
-  let list = dayTrendingMoviesList ? dayTrendingMoviesList : [];
+function TrendingMovies({ dayTrendingMoviesList, weekTrendingMoviesList, genres }) {
+  const [showWeekTrendingMovies, setShowWeekTrendingMovies] = useState(false);
+  let list;
+  if (showWeekTrendingMovies) {
+    list = weekTrendingMoviesList ? weekTrendingMoviesList : [];
+  } else {
+    list = dayTrendingMoviesList ? dayTrendingMoviesList : [];
+  }
 
   return (
     <div>
-      <h2>Trending</h2>
-      <div style={{ height: "30vh" }}>
+      <div className="d-flex flex-row">
+        <h2>Trending</h2>
+        <button className={`btn btn-${showWeekTrendingMovies ? "link" : "success"} ml-2`} onClick={() => { setShowWeekTrendingMovies(false) }}>Day</button>
+        <button className={`btn btn-${showWeekTrendingMovies ? "success" : "link"}`} onClick={() => { setShowWeekTrendingMovies(true) }}>Week</button>
+      </div>
+      <div style={{ height: "36rem", marginBottom: "10px" }}>
         <HomePageMovie list={list} genres={genres} />
       </div>
     </div>
@@ -17,7 +27,9 @@ function TrendingMovies({ dayTrendingMoviesList, genres }) {
 }
 
 TrendingMovies.propTypes = {
-
+  dayTrendingMoviesList: PropTypes.array.isRequired,
+  weekTrendingMoviesList: PropTypes.array.isRequired,
+  genres: PropTypes.object.isRequired,
 };
 
 export default TrendingMovies;
