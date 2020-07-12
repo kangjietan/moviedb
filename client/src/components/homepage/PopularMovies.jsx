9000b26143a78bab292079a7b2c41c7c211b6dde
kangjietan <kangjietan@gmail.com>
2020-07-11 18:27:44 -0700

    Add prop types, increase container height, and add bottom padding

--- client/src/components/homepage/PopularMovies.jsx
+++ client/src/components/homepage/PopularMovies.jsx
@@ -8,7 +8,7 @@ function PopularMovies({ popularMovieList, genres }) {
   return (
     <div>
       <h2>What's Popular</h2>
-      <div style={{ height: "35rem" }}>
+      <div style={{ height: "36rem", marginBottom: "10px" }}>
         <HomePageMovie list={list} genres={genres} />
       </div>
     </div>
@@ -16,7 +16,8 @@ function PopularMovies({ popularMovieList, genres }) {
 }
 
 PopularMovies.propTypes = {
-
+  popularMovieList: PropTypes.array.isRequired,
+  genres: PropTypes.object.isRequired,
 };
 
 export default PopularMovies;
