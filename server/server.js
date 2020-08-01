63720c667f1526d029335c00ba754970f5f6a752
kangjietan <kangjietan@gmail.com>
2020-07-31 18:59:07 -0700

    Bring in express-session and passport and add routes for user

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
 const express = require("express");
 
+const morgan = require("morgan");
+
+const passport = require("passport");
+
+const session = require("express-session");
+
 const path = require("path");
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(morgan("tiny"));
+
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // Routes
 app.use('/tmdb', require('./routes/tmdb'));
+
+app.use('/user', require('./routes/user'));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
