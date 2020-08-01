961a5e4830d624c4e796bc70e8321a4b7001aa90
kangjietan <kangjietan@gmail.com>
2020-08-01 16:35:06 -0700

    Add retrieve user movie lists actions

--- client/src/actions/movieActions.js
+++ client/src/actions/movieActions.js
@@ -1,5 +1,9 @@
 import * as actions from "../actions/types";
 
+import axios from 'axios';
+
+const serverUrl = "http://localhost:3000";
+
 export const addWatchedMovie = (movie) => (dispatch) => {
   dispatch({
     type: actions.ADD_WATCHED_MOVIE,
@@ -27,3 +31,25 @@ export const removeToWatchMovie = (movie) => (dispatch) => {
     payload: { id: movie.id },
   });
 };
+
+export const getUserWatchedList = () => (dispatch) => {
+  axios.get(`${serverUrl}/user/watchedlist`)
+  .then((response) => {
+    dispatch({
+      type: actions.GET_USER_WATCHED_LIST,
+      payload: response.data,
+    });
+  })
+  .catch((err) => console.log(err));
+}
+
+export const getUserToWatchList = () => (dispatch) => {
+  axios.get(`${serverUrl}/user/towatchlist`)
+  .then((response) => {
+    dispatch({
+      type: actions.GET_USER_TO_WATCH_LIST,
+      payload: response.data,
+    });
+  })
+  .catch((err) => console.log(err));
+}
