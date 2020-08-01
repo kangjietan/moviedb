cb95b72f2a54f1d93ec10e6d73235aa72c89cf00
kangjietan <kangjietan@gmail.com>
2020-08-01 16:18:09 -0700

    Import and use session reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import searchReducer from "./searchReducer";
 import movieReducer from "./movieReducer";
+import sessionReducer from "./sessionReducer";
 
 export default combineReducers({
   search: searchReducer,
   movie: movieReducer,
+  session: sessionReducer,
 });
