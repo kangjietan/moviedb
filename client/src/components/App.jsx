02c8dbff1630d59dae0ae8a75730c2dd7848b6e6
kangjietan <kangjietan@gmail.com>
2020-08-01 17:36:49 -0700

    Add new state from redux, prop types, and event listener on page reload/close

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { connect } from 'react-redux';
 import { genresFromAPI, popularMoviesFromAPI, trendingDayMoviesFromAPI, trendingWeekMoviesFromAPI } from '../actions/searchActions';
+
+import axios from 'axios';
+
+import qs from 'qs';
 
 import Navigation from './Navigation';
 import Home from './Home';
@@ -12,6 +17,7 @@ import ToWatchList from './ToWatchList';
 import Results from './Results';
 import Register from './Register';
 import Login from './Login';
+
 
 class App extends Component {
   constructor(props) {
@@ -24,6 +30,22 @@ class App extends Component {
     this.props.popularMoviesFromAPI();
     this.props.trendingDayMoviesFromAPI();
     this.props.trendingWeekMoviesFromAPI();
+
+    window.addEventListener('beforeunload', this.updateUserList);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.updateUserList);
+  }
+
+  updateUserList(event) {
+    event.preventDefault();
+    event.returnValue = false;
+    axios.post("/user/watchedlist/update")
+    .then((response) => {
+
+    })
+    .catch((err) => console.log(err));
   }
 
   render() {
@@ -47,8 +69,20 @@ class App extends Component {
 
 App.propTypes = {
   genresFromAPI: PropTypes.func.isRequired,
+  popularMoviesFromAPI: PropTypes.func.isRequired,
+  trendingDayMoviesFromAPI: PropTypes.func.isRequired,
+  trendingWeekMoviesFromAPI: PropTypes.func.isRequired,
+  watchedList: PropTypes.object.isRequired,
+  toWatchList: PropTypes.object.isRequired,
+  userLoggedIn: PropTypes.bool.isRequired,
 }
+
+const mapStateToProps = state => ({
+  watchedList: state.movie.watchedList,
+  toWatchList: state.movie.toWatchList,
+  userLoggedIn: state.session.userLoggedIn,
+});
 
 const mapDispatchToProps = { genresFromAPI, popularMoviesFromAPI, trendingDayMoviesFromAPI, trendingWeekMoviesFromAPI };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
