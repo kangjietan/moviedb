189b7af6e004a6c0608f1c54d50ba558e8c27874
kangjietan <kangjietan@gmail.com>
2020-08-01 17:26:48 -0700

    Formatting

--- client/src/actions/sessionActions.js
+++ client/src/actions/sessionActions.js
@@ -3,6 +3,6 @@ import * as actions from "../actions/types";
 export const setUserIsLoggedIn = (status) => (dispatch) => {
   dispatch({
     type: actions.SET_USER_IS_LOGGED_IN,
-    payload: status
+    payload: status,
   });
-}
+};
